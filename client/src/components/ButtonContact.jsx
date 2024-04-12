400403e74c2106deabbdbf019d4688166b105d08
Enzo Michel <enzomichel399@gmail.com>
2024-04-12 09:40:53 +0200

    last commit

---
+++ client/src/components/ButtonContact.jsx
@@ -0,0 +1,8 @@
+import '../styles/ButtonContact.scss'
+
+function ButtonContact () {
+    return (
+<a className='button' href="null">Contactez-nous</a>
+    )
+}
+export default ButtonContact
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import ButtonContact from "./ButtonContact";
+import '../styles/Footer.scss'
+
+function Footer () {
+    return (
+<footer>
+    <ButtonContact/>
+    <section>
+<a href="https://discord.com/">
+        <img src="/src/assets/images/discord-icon-svgrepo-com.svg" alt="icon discord"/>
+</a>
+<a href="https://slack.com/intl/fr-fr/">
+        <img src="/src/assets/images/slack-tile.svg" alt="icon slack"/>
+</a>
+    </section>
+<p>© 2024 Tous droits réservés. Le contenu de ce site web, y compris mais sans s'y limiter, le texte, les images, les graphiques et les logos, est la propriété intellectuelle de la société ABC et est protégé par les lois sur le droit d'auteur applicables. Toute utilisation non autorisée du contenu de ce site peut constituer une violation des lois sur le droit d'auteur et des droits de propriété intellectuelle, et entraîner des poursuites judiciaires.</p>
+</footer>
+    )
+}
+export default Footer;
