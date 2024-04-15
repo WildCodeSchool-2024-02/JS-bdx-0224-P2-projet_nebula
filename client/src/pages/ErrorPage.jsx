84f9b90a0163d5416d456478619be2e019b464bf
pjroggy <pierrejeanroggy@gmail.com>
2024-04-16 00:19:27 +0200

    add fix after first review

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -3,13 +3,13 @@ import "../styles/ErrorPage404.scss";
 
 function ErrorPage404() {
   return (
-    <section className="error404">
-      <h2>Trou de ver E404</h2>
-      <p>Cette destination n'existe pas</p>
+    <main className="error404">
+      <h1 className="titleError404">Trou de ver E404</h1>
+      <p className="textError404">Cette destination n'existe pas</p>
       <Link to="/" className="link">
         Retour au spatioport
       </Link>
-    </section>
+    </main>
   );
 }
 
