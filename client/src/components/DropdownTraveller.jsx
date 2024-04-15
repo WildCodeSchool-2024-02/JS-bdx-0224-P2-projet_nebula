cf3ad6b953dd0716f25691036ec6189dc2e80860
pjroggy <pierrejeanroggy@gmail.com>
2024-04-15 22:36:08 +0200

    add fix before first review

--- client/src/components/DropdownTraveller.jsx
+++ client/src/components/DropdownTraveller.jsx
@@ -9,31 +9,24 @@ function DropdownTraveller() {
   };
 
   return (
-    <section className="dropDownTravellerSection">
-      <label className="dropDownTravellerLabel" htmlFor="select-number-traveller">
+    <>
+      <label className="dropDownTravellerLabel" htmlFor="selectNumberTraveller">
         Nombre de passagers :
       </label>
       <select
         name="travellers"
-        id="select-number-traveller"
+        id="selectNumberTraveller"
         value={selectedOption}
         onChange={handelSelectChange}
         className="dropDownTravellerSelect"
       >
-        <option value={1} selected>
-          1
-        </option>
-        <option value={2}>2</option>
-        <option value={3}>3</option>
-        <option value={4}>4</option>
-        <option value={5}>5</option>
-        <option value={6}>6</option>
-        <option value={7}>7</option>
-        <option value={8}>8</option>
-        <option value={9}>9</option>
-        <option value={10}>10</option>
+        {[...Array(10)].map((_, index) => (
+          <option key={Array.id} value={index + 1}>
+            {index + 1}
+          </option>
+        ))}
       </select>
-    </section>
+    </>
   );
 }
 
