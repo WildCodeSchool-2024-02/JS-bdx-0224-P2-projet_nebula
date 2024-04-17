83a99a619451fb0131877e68879e03ad75349f7d
pjroggy <pierrejeanroggy@gmail.com>
2024-04-17 10:41:47 +0200

    delete app.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
+import ErrorPage404 from "./pages/ErrorPage";
 import Footer from "./components/Footer";
 import "./styles/app.scss";
 
 function App() {
-  return <Footer />;
+  return (
+    <>
+      <ErrorPage404 />
+      <Footer />;
+    </>
+  );
 }
 
 export default App;
