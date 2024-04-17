f62b52f0b519bcfeb0be968f050da33bd3b5f14d
NESRINERABAAOUI <161383698+NESRINERABAAOUI@users.noreply.github.com>
2024-04-17 19:54:34 +0200

    added the burger menu and updated the navbar display

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Navbar.scss";
 import Logo from "../assets/images/logo.svg";
+import { IoMdMenu, IoMdCloseCircle } from "react-icons/io";
 
 function Navbar({ setCurrentLocation }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <img src={Logo} />
-      <ul>
-        <li>
-          <a onClick={() => setCurrentLocation("/")}>Accueil</a>
-        </li>
-        <li>
-          <a onClick={() => setCurrentLocation("/vehicule")}>Vehicule</a>
-        </li>
-        <li>
-          <a onClick={() => setCurrentLocation("/info")}>Info traffic</a>
-        </li>
-        <li>
-          <a onClick={() => setCurrentLocation("/voyage")}>Votre voyage</a>
-        </li>
-      </ul>
+    <nav className={`navbar ${isOpen ? "open" : ""}`}>
+      <img src={Logo} alt="Logo" />
+      <div>
+        <a
+          className={isOpen ? "menu1" : "menu"}
+          onClick={toggleMenu}
+          src={Logo}
+          alt="Menu"
+        >
+          {isOpen ? <IoMdCloseCircle /> : <IoMdMenu />}
+        </a>
+        <ul className={`menu-items ${isOpen ? "open" : ""}`}>
+          <li className="close" onClick={toggleMenu}>
+            X
+          </li>
+          <li>
+            <a onClick={() => setCurrentLocation("/")}>Accueil</a>
+          </li>
+          <li>
+            <a onClick={() => setCurrentLocation("/vehicule")}>Vehicule</a>
+          </li>
+          <li>
+            <a onClick={() => setCurrentLocation("/info")}>Info traffic</a>
+          </li>
+          <li>
+            <a onClick={() => setCurrentLocation("/voyage")}>Votre voyage</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
