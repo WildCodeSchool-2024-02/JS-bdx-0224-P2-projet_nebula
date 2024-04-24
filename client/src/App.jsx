28a10f25fdbd560881e03ad15ddac1e7c71d1456
NESRINERABAAOUI <161383698+NESRINERABAAOUI@users.noreply.github.com>
2024-04-24 23:39:11 +0200

    ajout images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,21 +5,21 @@ import Home from "./components/Home";
 import Vehicule from "./components/Vehicule";
 import Info from "./components/Info";
 import Voyage from "./components/Voyage";
+import VehiculesDb from "./db/VehiculesDb";
 
 function App() {
   const [currentLocation, setCurrentLocation] = useState("/");
-  const [vehicules, setVehicules] = useState();
-
+  /* const [vehicules, setVehicules] = useState();
   useEffect(() => {
     fetch("https://api.star-citizen.wiki/api/vehicles?limit=10")
       .then((response) => response.json())
       .then((data) => setVehicules(data.data));
-  }, []);
+  }, []);  */
   return (
     <div className="container">
       <Navbar setCurrentLocation={setCurrentLocation} />
       {currentLocation === "/" && <Home />}
-      {currentLocation === "/vehicule" && <Vehicule vehicules={vehicules} />}
+      {currentLocation === "/vehicule" && <Vehicule vehicules={VehiculesDb} />}
       {currentLocation === "/info" && <Info />}
       {currentLocation === "/voyage" && <Voyage />}
     </div>
--- client/src/components/Vehicule.jsx
+++ client/src/components/Vehicule.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import PropTypes from "prop-types";
 import "../styles/Vehicules.scss";
 import vehiculeBg from "../assets/images/ship.jpeg";
 
-const Vehicule = ({ vehicules }) => {
-  console.log(vehicules);
+function Vehicule({ vehicules }) {
+  console.info(vehicules);
   return (
-    <div className="main">
-      <img src={vehiculeBg} className="background" />
+    <main className="main">
+      <img src={vehiculeBg} className="background" alt="vehicule" />
       <div className="cards">
         {vehicules.map((vehicule) => (
-          <div className="card">
-            <h2>Name: {vehicule.name} </h2>
+          <div key={vehicule.id} className="card">
+            <img className="vehiculeImg" src={vehicule.img} alt="img" />
+            <h2 className="vehiculeName">{vehicule.nom} </h2>
+            <h4>desc: {vehicule.description} </h4>
+            <div className="carac">
+              <h3>Speed: {vehicule.speed} </h3>
+              <h3>Agility: {vehicule.agility} </h3>
+            </div>
           </div>
         ))}
       </div>
-    </div>
+    </main>
   );
+}
+Vehicule.propTypes = {
+  vehicules: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      // Ajoutez d'autres propriétés et leurs types si nécessaire
+    }),
+  ).isRequired,
 };
-
 export default Vehicule;
