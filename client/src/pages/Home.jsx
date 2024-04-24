bee1f93232852c52232518e177c0a76f7f7deefb
Enzo Michel <enzomichel399@gmail.com>
2024-04-24 10:02:52 +0200

    fix fetc

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TopPlanet from "../components/TopPlanet";
 import HomeCards from "../components/HomeCards";
 import Slogan from "../components/Slogan";
@@ -7,65 +7,57 @@ import { GalactapediaProvider } from "../Contexts/GalactapediaContext";
 import "../styles/app.scss";
 
 function Home() {
-  const [thumbnailUrl1, setThumbnailUrl1] = useState(" ");
-  const [titleUrl1, setTitleUrl1] = useState(" ");
+  const [thumbnail1, setThumbnail1] = useState({ url: "", title: "" });
 
-  const [thumbnailUrl2, setThumbnailUrl2] = useState(" ");
-  const [titleUrl2, setTitleUrl2] = useState(" ");
+  const [thumbnailUrl2, setThumbnailUrl2] = useState({url : "",title: ""});
 
-  const [thumbnailUrl3, setThumbnailUrl3] = useState(" ");
-  const [titleUrl3, setTitleUrl3] = useState(" ");
+  const [thumbnailUrl3, setThumbnailUrl3] = useState({url : "",title: ""});
 
-  const [thumbnailUrl4, setThumbnailUrl4] = useState(" ");
-  const [titleUrl4, setTitleUrl4] = useState(" ");
+  const [thumbnailUrl4, setThumbnailUrl4] = useState({url : "",title: ""});
 
-  const [thumbnailUrl5, setThumbnailUrl5] = useState(" ");
-  const [titleUrl5, setTitleUrl5] = useState(" ");
+  const [thumbnailUrl5, setThumbnailUrl5] = useState({url : "",title: ""});
 
-  const [thumbnailUrl6, setThumbnailUrl6] = useState(" ");
-  const [titleUrl6, setTitleUrl6] = useState(" ");
+  const [thumbnailUrl6, setThumbnailUrl6] = useState({url : "",title: ""});
 
-  fetch("https://api.star-citizen.wiki/api/galactapedia/0Qlx4dQnxL")
+  useEffect(() => {
+    fetch("https://api.star-citizen.wiki/api/galactapedia/0Qlx4dQnxL")
     .then((response) => response.json())
     .then((data) => {
-      setThumbnailUrl1(data.data.thumbnail);
-      setTitleUrl1(data.data.title);
+      setThumbnail1({ url: data.data.thumbnail, title: data.data.title });
     });
 
   fetch("https://api.star-citizen.wiki/api/galactapedia/RegWGPlxqy")
     .then((response) => response.json())
     .then((data) => {
-      setThumbnailUrl2(data.data.thumbnail);
-      setTitleUrl2(data.data.title);
+      setThumbnailUrl2({ url: data.data.thumbnail, title: data.data.title });
     });
 
   fetch("https://api.star-citizen.wiki/api/galactapedia/RX3rnQ3XqM")
     .then((response) => response.json())
     .then((data) => {
-      setThumbnailUrl3(data.data.thumbnail);
-      setTitleUrl3(data.data.title);
+      setThumbnailUrl3({ url: data.data.thumbnail, title: data.data.title });
     });
 
   fetch("https://api.star-citizen.wiki/api/galactapedia/VarAoYEQYd")
     .then((response) => response.json())
     .then((data) => {
-      setThumbnailUrl4(data.data.thumbnail);
-      setTitleUrl4(data.data.title);
+      setThumbnailUrl4({ url: data.data.thumbnail, title: data.data.title });
     });
 
   fetch("https://api.star-citizen.wiki/api/galactapedia/RkGGjdOwQz")
     .then((response) => response.json())
     .then((data) => {
-      setThumbnailUrl5(data.data.thumbnail);
-      setTitleUrl5(data.data.title);
+      setThumbnailUrl5({ url: data.data.thumbnail, title: data.data.title });
     });
 
   fetch("https://api.star-citizen.wiki/api/galactapedia/0qaPo2JOj1")
     .then((response) => response.json())
     .then((data) => {
-      setThumbnailUrl6(data.data.thumbnail);
-      setTitleUrl6(data.data.title);
+      setThumbnailUrl6({ url: data.data.thumbnail, title: data.data.title });
     });
+  }, []);
+
+  
 
   return (
     <>
@@ -79,21 +71,21 @@ function Home() {
       </GalactapediaProvider>
       <HomeCards
         sectionTitle="Populaires"
-        planetTitleLeft={titleUrl1}
-        planetTitleMid={titleUrl2}
-        planetTitleRight={titleUrl3}
-        imageLeft={thumbnailUrl1}
-        imageMid={thumbnailUrl2}
-        imageRight={thumbnailUrl3}
+        planetTitleLeft={thumbnail1.title}
+        planetTitleMid={thumbnailUrl2.title}
+        planetTitleRight={thumbnailUrl3.title}
+        imageLeft={thumbnail1.url}
+        imageMid={thumbnailUrl2.url}
+        imageRight={thumbnailUrl3.url}
       />
       <HomeCards
         sectionTitle="Proches de vous"
-        planetTitleLeft={titleUrl4}
-        planetTitleMid={titleUrl5}
-        planetTitleRight={titleUrl6}
-        imageLeft={thumbnailUrl4}
-        imageMid={thumbnailUrl5}
-        imageRight={thumbnailUrl6}
+        planetTitleLeft={thumbnailUrl4.title}
+        planetTitleMid={thumbnailUrl5.title}
+        planetTitleRight={thumbnailUrl6.title}
+        imageLeft={thumbnailUrl4.url}
+        imageMid={thumbnailUrl5.url}
+        imageRight={thumbnailUrl6.url}
       />
       <Slogan />
     </>
