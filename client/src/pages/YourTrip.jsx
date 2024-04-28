5582689328d0fff3b719ecbf4247bfd11285c3d6
Rollet Fabien <fabrol47@gmail.com>
2024-04-28 11:59:32 +0200

    Css ticket done

--- client/src/pages/YourTrip.jsx
+++ client/src/pages/YourTrip.jsx
@@ -18,18 +18,15 @@ export default function YourTrip() {
       <JourneyDetails />
       <section className="yourTrip">
         <h2>Your ticket</h2>
-        <div className="summaryTicket">
+        <article className="summaryTicket">
           <ul>
-            {/* <li>{selectedDeparture}</li>
-            <li>{selectedArrival}</li> */}
             <li>From: {selectedDeparture}</li>
             <li>To: {selectedArrival}</li>
             <li>{selectedTravelers} passenger(s)</li>
             <li>{selectedTripType}</li>
             <li>{selectedDate}</li>
-            {/* <li>{selectedDate}</li> */}
           </ul>
-        </div>
+        </article>
         <p>Click here to download your ticket :</p>
         <button type="button" aria-label="Download">
           Download
