75c085d8a4ef387a94e0be7e1ef2523c03939c0d
NESRINERABAAOUI <161383698+NESRINERABAAOUI@users.noreply.github.com>
2024-04-29 14:51:31 +0200

    dimension du vehicule

--- client/src/components/Vehicule.jsx
+++ client/src/components/Vehicule.jsx
@@ -1,35 +1,8 @@
 import PropTypes from "prop-types";
 import ReactPlayer from "react-player";
-import { Slide } from "react-slideshow-image";
-
 import "../styles/Vehicules.scss";
-import "react-slideshow-image/dist/styles.css";
-
-const equipementsIMG = [
-  {
-    url: "https://media.robertsspaceindustries.com/sctdirqpsna8r/slideshow.jpg",
-    caption: "Helmet 1",
-    favorite: true,
-  },
-  {
-    url: "https://media.robertsspaceindustries.com/l5cr40lfxfj2e/slideshow.jpg",
-    caption: "Helmet 2",
-    favorite: false,
-  },
-  {
-    url: "https://media.robertsspaceindustries.com/ymfc96ij31rfz/slideshow.jpg",
-    caption: "Helmet 3",
-    favorite: false,
-  },
-  {
-    url: "https://media.robertsspaceindustries.com/ymfc96ij31rfz/slideshow.jpg",
-    caption: "Helmet 3",
-    favorite: false,
-  },
-];
 
 function Vehicule({ vehicules }) {
-  const handleFavClicked = () => {};
   return (
     <main className="mainOurDestination">
       <ReactPlayer
@@ -40,11 +13,11 @@ function Vehicule({ vehicules }) {
 
       <section className="cards">
         {vehicules.map((vehicule) => (
-          <div key={vehicule.id} className="cardVehicule">
+          <section key={vehicule.id} className="cardVehicule">
             <img className="vehiculeImg" src={vehicule.img} alt="img" />
             <h2 className="vehiculeName">{vehicule.nom} </h2>
-            <h4>Description: {vehicule.description} </h4>
-            <div className="carac">
+            <p>Description: {vehicule.description} </p>
+            <article className="carac">
               <p>
                 Speed <br /> {vehicule.speed}{" "}
               </p>
@@ -54,31 +27,9 @@ function Vehicule({ vehicules }) {
               <p>
                 Size <br /> {vehicule.Size}{" "}
               </p>
-            </div>
-          </div>
+            </article>
+          </section>
         ))}
-      </section>
-      <section className="generic">
-        <h1 className="vehiculeName">Equipments</h1>
-        <div className="divWidth">
-          <Slide>
-            {equipementsIMG.map((slideImage) => (
-              <div className="generic" key={slideImage.caption}>
-                <img
-                  src={slideImage.url}
-                  className="divStyle"
-                  alt="helmetImg"
-                />
-                <span className="spanStyle">
-                  {slideImage.caption}{" "}
-                  <button type="button" onClick={() => handleFavClicked}>
-                    {slideImage.favorite ? "❤️" : "🖤"}
-                  </button>
-                </span>
-              </div>
-            ))}
-          </Slide>
-        </div>
       </section>
     </main>
   );
@@ -89,7 +40,6 @@ Vehicule.propTypes = {
       id: PropTypes.number.isRequired,
       name: PropTypes.string.isRequired,
       isFav: PropTypes.bool.isRequired,
-      // Ajoutez d'autres propriétés et leurs types si nécessaire
     }),
   ).isRequired,
 };
--- client/src/db/VehiculesDb.js
+++ client/src/db/VehiculesDb.js
@@ -6,7 +6,7 @@ const vehicles = [
       "The 600i is a luxurious touring vessel designed for comfort and style. With its sleek exterior and spacious interior, it's the epitome of elegance among spacecraft. Despite its opulent amenities, it maintains impressive speed and agility, making it ideal for both leisurely cruises and swift journeys across the galaxy.",
     speed: "Fast",
     agility: "Moderate",
-    Size: "large",
+    Size: "CAPITAL",
     img: "https://media.starcitizen.tools/thumb/8/80/600i_Landed_on_luxury_Island_-_Isometric.jpg/1920px-600i_Landed_on_luxury_Island_-_Isometric.jpg",
   },
   {
@@ -16,7 +16,7 @@ const vehicles = [
       "The 400i strikes a balance between luxury and functionality. Its streamlined design not only exudes sophistication but also enhances performance, allowing for smooth navigation through space. With its advanced propulsion systems, the 400i offers impressive speed and agility, making it a versatile choice for exploration and transportation.",
     speed: "High",
     agility: "High",
-    Size: "Capital",
+    Size: "CAPITAL",
     img: "https://media.starcitizen.tools/thumb/b/b9/400i_-_Landed_Loaded_with_personal_luggage.png/1920px-400i_-_Landed_Loaded_with_personal_luggage.png",
   },
   {
@@ -26,7 +26,7 @@ const vehicles = [
       "The 890 Jump is the pinnacle of luxury among spacecraft, offering unparalleled comfort and opulence. Its lavish interiors rival those of the most extravagant estates, providing a truly indulgent experience for its passengers. Despite its grandeur, the 890 Jump doesn't sacrifice speed or agility, ensuring swift and graceful travels through the cosmos.",
     speed: "Moderate",
     agility: "Low",
-    Size: "Medium",
+    Size: "MEDIUM",
     img: "https://media.starcitizen.tools/thumb/b/bd/890Jump_beach.jpg/1920px-890Jump_beach.jpg",
   },
   {
@@ -36,7 +36,7 @@ const vehicles = [
       "The 100i is a sleek and nimble spacecraft designed for solo or small crew exploration missions. Its compact size belies its capabilities, as it boasts impressive speed and agility for swift traversal of space. With its minimalist yet functional interior, the 100i is perfect for those seeking adventure without sacrificing comfort.",
     speed: "High",
     agility: "High",
-    Size: "Small",
+    Size: "SMALL",
     img: "https://media.starcitizen.tools/thumb/b/b8/100i_Sunset.jpg/1920px-100i_Sunset.jpg",
   },
   {
@@ -45,6 +45,7 @@ const vehicles = [
     description:
       "The Terrapin is a rugged and durable spacecraft built for long-range reconnaissance and exploration missions. Its robust hull and advanced sensor suite make it ideal for traversing hazardous environments with ease. While it may not match the speed of sleeker vessels, the Terrapin more than compensates with its unmatched durability and resilience.",
     speed: "Moderate",
+    Size: "CAPITAL",
     agility: "Low",
     img: "https://media.starcitizen.tools/thumb/a/a5/Terrapin_in_SelfLand_-_Isometric.jpg/1920px-Terrapin_in_SelfLand_-_Isometric.jpg",
   },
@@ -55,7 +56,7 @@ const vehicles = [
       "The Carrack is a versatile and formidable exploration vessel, capable of undertaking extended missions into uncharted territories. Its spacious interior accommodates a wide range of amenities, including research facilities and living quarters for extended stays. Despite its size, the Carrack maintains impressive speed and agility, ensuring efficient exploration of even the most remote regions of space.",
     speed: "Moderate",
     agility: "Moderate",
-    Size: "Small",
+    Size: "CAPITAL",
     img: "https://media.starcitizen.tools/thumb/c/cd/EXT_Carrack_Microtech_112019-Min.jpg/1920px-EXT_Carrack_Microtech_112019-Min.jpg",
   },
   {
@@ -65,7 +66,7 @@ const vehicles = [
       "The Constellation Aquila is a versatile multi-crew exploration vessel equipped for long-duration missions. Its modular design allows for easy customization to suit various mission profiles, from exploration to commerce. With its combination of speed, firepower, and versatility, the Aquila is a dependable choice for those seeking adventure beyond the stars.",
     speed: "High",
     agility: "Moderate",
-    Size: "Small",
+    Size: "SMALL",
     img: "https://media.starcitizen.tools/thumb/2/20/Constellation_Aquila_-_Flying_over_world.jpg/1280px-Constellation_Aquila_-_Flying_over_world.jpg",
   },
   {
@@ -75,7 +76,7 @@ const vehicles = [
       "The Starfarrer is a rugged and reliable fuel transporter, essential for supporting long-range exploration missions. Its massive fuel tanks ensure extended operational range, making it indispensable for journeys into the unknown. While not built for speed or agility, the Starfarrer excels in its role as a dependable workhorse of the stars.",
     speed: "Low",
     agility: "Low",
-    Size: "Small",
+    Size: "SMALL",
     img: "https://media.starcitizen.tools/2/2a/Philip-howlett-starfarer-final-02.jpg",
   },
   {
@@ -85,7 +86,7 @@ const vehicles = [
       "The E1 Spirit is a versatile and agile spacecraft suitable for a variety of missions, from exploration to light transport. Its sleek design and advanced maneuvering thrusters make it ideal for navigating densely populated space lanes or exploring remote star systems. With its compact yet functional interior, the E1 Spirit offers a perfect balance of speed, agility, and versatility.",
     speed: "High",
     agility: "High",
-    Size: "Small",
+    Size: "MEDIUM",
     img: "https://media.starcitizen.tools/thumb/9/9e/Spirit_E1_landed_with_passengers_and_luggage.jpg/1920px-Spirit_E1_landed_with_passengers_and_luggage.jpg",
   },
   {
@@ -95,7 +96,7 @@ const vehicles = [
       "The C2 Hercules is a massive cargo transporter designed to haul heavy loads across the galaxy. Its cavernous cargo bay can accommodate a wide variety of goods, from vehicles to bulk materials. Despite its size, the C2 Hercules is surprisingly agile for its class, allowing it to navigate through spaceports and planetary atmospheres with ease.",
     speed: "Moderate",
     agility: "Low",
-    Size: "Small",
+    Size: "SMALL",
     img: "https://media.starcitizen.tools/thumb/c/cc/C2_Feature_Style_clouds.jpg/1920px-C2_Feature_Style_clouds.jpg",
   },
 ];
